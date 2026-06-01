bd27529b7ee6cdc78143aba3956172f0d8578938
Gabriel Lima de Oliveira <ogabriellima1999@gmail.com>
2026-05-31 23:20:39 -0300

    fix/bugs in invoice

--- backend/src/main/java/br/com/SmartPDV/SmartPDV/DTOs/ResponseDTOs/LojaResponse.java
+++ backend/src/main/java/br/com/SmartPDV/SmartPDV/DTOs/ResponseDTOs/LojaResponse.java
@@ -1,50 +1,21 @@
 package br.com.SmartPDV.SmartPDV.DTOs.ResponseDTOs;
 
-public class LojaResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LojaResponse {
+	private Long id;
 	private String razaoSocial;
 	private String cnpj;
 	private String IE;
 	private String endereco;
 
-	public LojaResponse(String razaoSocial, String cnpj, String iE, String endereco) {
-		super();
-		this.razaoSocial = razaoSocial;
-		this.cnpj = cnpj;
-		IE = iE;
-		this.endereco = endereco;
-	}
-
-	public String getRazaoSocial() {
-		return razaoSocial;
-	}
-
-	public void setRazaoSocial(String razaoSocial) {
-		this.razaoSocial = razaoSocial;
-	}
-
-	public String getCnpj() {
-		return cnpj;
-	}
-
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
-	}
-
-	public String getIE() {
-		return IE;
-	}
-
-	public void setIE(String iE) {
-		IE = iE;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
+	
 
 }
